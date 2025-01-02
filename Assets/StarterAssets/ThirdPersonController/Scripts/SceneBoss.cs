392705f780ac4a2a88abf3a5bac4c938a22fa539
Jacob Palin <90143523+jacobpalin@users.noreply.github.com>
2025-01-01 20:22:37 -0800

    Revert "UI"
    This reverts commit 7a55cf81012edc13897a8779784b386f0ad134ff.

--- Assets/StarterAssets/ThirdPersonController/Scripts/SceneBoss.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/SceneBoss.cs
@@ -23,11 +23,8 @@ public class SceneBoss : MonoBehaviour
         SceneManager.LoadScene("GameScene");
     }
 
-    public void MainMenu() {
-        SceneManager.LoadScene("Main Menu");
-    }
-
     public void Credits() {
+        Debug.Log("Hoe");
         creditsInfo.SetActive(!active);
         active = !active;
 
